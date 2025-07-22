b7562724bfba03e36d4b9ce433f8b395d402d75d
SerGio <foresterares@gmail.com>
2025-07-22 17:30:25 +0300

    почті все

--- backend/app.py
+++ backend/app.py
@@ -123,4 +123,6 @@ def get_chart_image():
         return jsonify({'error': 'Карта ще не створена'}), 404
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
