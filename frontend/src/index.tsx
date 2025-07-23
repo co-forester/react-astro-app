ba8043f51a4cd1edbdaabe3a9d64eee4025f4556
SerGio <foresterares@gmail.com>
2025-07-23 11:28:14 +0300

    пашті

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {RouterProvider} from "react-router-dom";
 import {Provider} from "react-redux";
+import { injectSpeedInsights } from "@vercel/speed-insights";
+
+injectSpeedInsights();
 
 import './index.css';
 import {router} from "./router";
