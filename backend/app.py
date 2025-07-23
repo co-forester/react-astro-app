0ecd43f7558737c57683adf1357010bbc900e741
SerGio <foresterares@gmail.com>
2025-07-23 16:10:54 +0300

    asded

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify, send_file
 from flask_cors import CORS
 from flatlib.chart import Chart
@@ -123,6 +124,6 @@ def get_chart_image():
         return jsonify({'error': 'Карта ще не створена'}), 404
 
 if __name__ == '__main__':
-    import os
+    
     port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
