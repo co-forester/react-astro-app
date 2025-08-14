4d2eb8cea3254bbfedaa6bd58f20a3a00b2eb552
SerGio <foresterares@gmail.com>
2025-08-14 12:07:27 +0300

    мрм

--- backend/app.py
+++ backend/app.py
@@ -137,5 +137,6 @@ def get_chart_image(filename):
 
 
 if __name__ == '__main__':
+    # Для локального запуску, якщо треба
     port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
