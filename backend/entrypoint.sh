7e720721b772a70f32fd6fef32ae0a3c8698c916
SerGio <foresterares@gmail.com>
2025-08-17 16:22:43 +0300

    вивививи

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 
-if [ ! -f /data/ephe/sepl_18.se1 ]; then
+# Перевіряємо, чи є хоча б один .se1 файл або інші ефемериди в /data/ephe
+if [ -z "$(find /data/ephe -type f -name '*.se1' | head -n 1)" ]; then
     echo "Ефемериди не знайдені у volume, розпаковуємо..."
     mkdir -p /data/ephe
     tar -xzf /app/ephe.tar.gz -C /data/ephe
@@ -9,4 +10,5 @@ else
     echo "Ефемериди знайдені у volume. Пропускаємо розпаковку."
 fi
 
+# Запускаємо основну команду (Gunicorn)
 exec "$@"
