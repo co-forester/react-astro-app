23356c8719b3610ebee309ae4c6a9f36647315eb
SerGio <foresterares@gmail.com>
2025-08-17 16:05:22 +0300

    jvbjv

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 
-# Якщо в volume /data/ephe нічого немає — розпаковуємо архів
 if [ ! -f /data/ephe/sepl_18.se1 ]; then
     echo "Ефемериди не знайдені у volume, розпаковуємо..."
     mkdir -p /data/ephe
@@ -10,5 +9,4 @@ else
     echo "Ефемериди знайдені у volume. Пропускаємо розпаковку."
 fi
 
-# Запускаємо основну команду (Gunicorn)
 exec "$@"
