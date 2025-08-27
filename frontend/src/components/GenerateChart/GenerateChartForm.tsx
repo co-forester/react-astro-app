341cef13882e507ccb56aa47788cad0db851ad7d
SerGio <foresterares@gmail.com>
2025-08-27 16:22:50 +0300

    000016

--- frontend/src/components/GenerateChart/GenerateChartForm.tsx
+++ frontend/src/components/GenerateChart/GenerateChartForm.tsx
@@ -53,14 +53,14 @@ const GenerateChartForm: React.FC = () => {
   };
 
   return (
-    <div className="formContainer fadeInForm">
+    <div className="css.formContainer fadeInForm">
       <form onSubmit={handleSubmit}>
         <input
           type="text"
           name="name"
           value={formData.name}
           onChange={handleChange}
-          className="formInput"
+          className="css.formInput"
           placeholder="Ім'я"
           required
         />
@@ -69,7 +69,7 @@ const GenerateChartForm: React.FC = () => {
           name="date"
           value={formData.date}
           onChange={handleChange}
-          className="formInput"
+          className="css.formInput"
           required
         />
         <input
@@ -77,7 +77,7 @@ const GenerateChartForm: React.FC = () => {
           name="time"
           value={formData.time}
           onChange={handleChange}
-          className="formInput"
+          className="css.formInput"
           required
         />
         <input
@@ -85,11 +85,11 @@ const GenerateChartForm: React.FC = () => {
           name="place"
           value={formData.place}
           onChange={handleChange}
-          className="formInput"
+          className="css.formInput"
           placeholder="Місто"
           required
         />
-        <button type="submit" className="formButton" disabled={loading}>
+        <button type="submit" className="css.formButton" disabled={loading}>
           {loading ? "Генерація..." : "Побудувати натальну карту"}
         </button>
       </form>
@@ -97,14 +97,14 @@ const GenerateChartForm: React.FC = () => {
       {error && <p style={{ color: "red", marginTop: "1rem" }}>{error}</p>}
 
       {chartUrl && (
-        <div className="chartContainer fadeInUpForm">
+        <div className="css.chartContainer fadeInUpForm">
           <h3>Натальна карта</h3>
           <img src={chartUrl} alt="Натальна карта" />
         </div>
       )}
 
       {aspectsJson && (
-        <div className="aspectsContainer fadeInUpForm">
+        <div className="css.aspectsContainer fadeInUpForm">
           <h3>Аспекти</h3>
           <table>
             <thead>
